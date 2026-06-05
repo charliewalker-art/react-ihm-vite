a003b1389afe29e9dcfb92ae168eff38db5eef91
Charlie <charliewaltert0206@gmail.com>
2026-06-05 13:31:43 +0300

    correction login api

--- src/hooks/useUtilisateur.ts
+++ src/hooks/useUtilisateur.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { UtilisateurRequest, UtilisateurResponse } from "../types/utilisateur";
 import type { UpdateProfilRequest } from "../types/profil";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const getHeaders = () => ({
   headers: {
