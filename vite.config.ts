8bf35e990263e4f6a1b96eff36f8271caea79c65
Charlie <charliewaltert0206@gmail.com>
2026-07-12 18:15:47 +0300

    fix vite config2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/resto/',
   plugins: [react(), tailwindcss()],
   define: {
     global: 'globalThis',
