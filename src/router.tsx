45be8b2c2312a5304cc1581e90f08249fd75b631
Charlie <charliewaltert0206@gmail.com>
2026-07-12 19:01:20 +0300

    fix vite config4

--- src/router.tsx
+++ src/router.tsx
@@ -77,4 +77,6 @@ export const router = createBrowserRouter([
     element: <ProtectedRoute><ProfilPage /></ProtectedRoute>,
   },
   { path: "*", element: <NotFound /> },
-]);
+], {
+  basename: "/resto",
+});
